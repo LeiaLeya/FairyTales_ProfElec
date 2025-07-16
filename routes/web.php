99cb4fddbad008445b6600c0f4d1cd1732f13434
LeiaLeya <leyangbaclayo1@gmail.com>
2025-07-16 13:25:55 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CharacterController;
-
+Route::get('/', function () {
+    return redirect('/characters');
+});
 Route::get('/characters', [CharacterController::class, 'index'])->name('characters.index');
 Route::get('/characters/upload', [CharacterController::class, 'uploadForm'])->name('characters.upload');
 Route::post('/characters/upload', [CharacterController::class, 'uploadImage'])->name('characters.upload.post');
