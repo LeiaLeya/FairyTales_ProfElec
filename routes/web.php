05e3eeab43802c3d0ab32250b06dab31abe8162f
LeiaLeya <leyangbaclayo1@gmail.com>
2025-07-16 13:37:08 +0800

    Trigger redeploy

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CharacterController;
 Route::get('/', function () {
     return redirect('/characters');
 });
+
 Route::get('/characters', [CharacterController::class, 'index'])->name('characters.index');
 Route::get('/characters/upload', [CharacterController::class, 'uploadForm'])->name('characters.upload');
 Route::post('/characters/upload', [CharacterController::class, 'uploadImage'])->name('characters.upload.post');
